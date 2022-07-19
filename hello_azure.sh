3243b72ce753f9d3a43e24565f3be93ddd187a68
Jackie Tung <jackie@outerbounds.co>
2022-07-19 09:30:58 -0700

    Azure Storage CI extras

---
+++ hello_azure.sh
@@ -0,0 +1,10 @@
+set -x
+
+export METAFLOW_DATASTORE_SYSROOT_AZURE=test-azure-storage/hello-azure-demo
+export METAFLOW_AZURE_STORAGE_ACCOUNT_URL=https://obbenchmark1.blob.core.windows.net/
+# export METAFLOW_AZURE_STORAGE_SHARED_ACCESS_SIGNATURE=
+
+METAFLOW_AZURE_STORAGE_WORKLOAD_TYPE=general python helloworld.py --datastore=azure run
+METAFLOW_AZURE_STORAGE_WORKLOAD_TYPE=high_throughput python helloworld.py --datastore=azure run
+
+
